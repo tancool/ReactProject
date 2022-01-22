0f742bd396e6807d971c53ee738bb0d0bd0d359d
Tank <tangege@outlook.com>
2022-01-22 15:36:23 +0800

    配置jira-dev-tool

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthProvider } from "./auth-context";
 export const AppProviders = ({ children }: { children: ReactNode }) => {
     // 未来所有的App级别的Providers都会在这里面添加
     // return <AuthProvider children={children} /> // 这种写法是同样的方式
 
     // 这样相对来说,是比较容易阅读的.
-    return <AuthProvider> 
-        {children}
-    </AuthProvider>
+    return (
+        <QueryClientProvider client={new QueryClient()}>
+            <AuthProvider>
+                {children}
+            </AuthProvider>
+        </QueryClientProvider>
 
+    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { loadDevTools } from 'jira-dev-tool'
+import { loadServer, DevTools } from 'jira-dev-tool'
 
 // 务必在 jira-dev-tool 引入 => 因为需要自定义主题变量. => 通过配置变量
 import 'antd/dist/antd.less'
 
 import { AppProviders } from 'context';
-loadDevTools(() => {
+loadServer(() => {
   ReactDOM.render(
     <React.StrictMode>
       <AppProviders>
+        <DevTools />
         <App />
       </AppProviders>
     </React.StrictMode>,
     document.getElementById('root')
-  );
+  )
 })
 
 // If you want to start measuring performance in your app, pass a function
